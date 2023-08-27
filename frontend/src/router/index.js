9da1620399a5b7a35268a3b9e2d12e077f601dca
Hunter Wang <hunter9812@qq.com>
2023-08-27 19:59:24 +0800

    change url

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,24 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+const routes = [
+    {
+        path: "/",
+        name: "home",
+        component: () => import("../views/AnalysisView.vue"),
+    },
+    {
+        path: "/analysis",
+        name: "analysis",
+        component: () => import("../views/AnalysisView.vue"),
+    },
+    {
+        path: "/about",
+        name: "about",
+        component: () => import("../views/AboutView.vue"),
+    },
+];
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: () => import("../views/AnalysisView.vue"),
-    },
-    {
-      path: "/analysis",
-      name: "analysis",
-      component: () => import("../views/AnalysisView.vue"),
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: () => import("../views/AboutView.vue"),
-    },
-  ],
-});
-
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
+})
 export default router;
--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const request = axios.create({
     // baseURL: "http://localhost:8000",//test
-    baseURL: "http://localhost:8090",//deploy
+    baseURL: "http://121.37.88.194:8000",//deploy
 });
 
 export default request;
